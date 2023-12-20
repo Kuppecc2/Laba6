7d4d1719a775a560819815381340bb71056c4890
Egor Kupcov <kupcovegor150@gmail.com>
2023-12-20 16:29:59 +0500

    Added documents from doxygen

---
+++ documentations/html/search/functions_1.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['operator_2a_0',['operator*',['../mycomplex_8cpp.html#a2518193c907c8db8fe5920468f8d6771',1,'mycomplex.cpp']]],
+  ['operator_2b_1',['operator+',['../mycomplex_8cpp.html#a4b1807e2221e04ba8661276644a7eacb',1,'mycomplex.cpp']]],
+  ['operator_2d_2',['operator-',['../mycomplex_8cpp.html#ad9363d22ed922d55de7e817eddf90ec0',1,'mycomplex.cpp']]],
+  ['operator_3c_3c_3',['operator&lt;&lt;',['../mycomplex_8cpp.html#a087b35621ae8bcdc365a7329a86c847a',1,'mycomplex.cpp']]],
+  ['operator_3e_3e_4',['operator&gt;&gt;',['../mycomplex_8cpp.html#a42f0189696ad5089d4ecdb84923ebd88',1,'mycomplex.cpp']]]
+];
---
+++ documentations/html/search/searchdata.js
@@ -0,0 +1,24 @@
+var indexSectionsWithContent =
+{
+  0: "cmot",
+  1: "c",
+  2: "mt",
+  3: "mo"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "files",
+  3: "functions"
+};
+
+var indexSectionLabels =
+{
+  0: "Указатель",
+  1: "Классы",
+  2: "Файлы",
+  3: "Функции"
+};
+
